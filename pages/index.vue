e0bc66917a2b87861dd3a55c79b407e9cace7a54
ghifarirmdn <ghifariramadhan2001@gmail.com>
2023-08-06 21:33:58 +0700

    first push

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Nuxt Routing set up successfully!</h1>
+    <h1 class="text-3xl font-bold underline text-purple">Hello world!</h1>
+    <NuxtLink to="/about">ke halaman about</NuxtLink>
+  </div>
+</template>
